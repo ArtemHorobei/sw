9fa464a90a537af1f78251d06ecc7051f2657bc0
artem.horobei <artem.horobei@yalantis.com>
2018-09-03 15:10:20 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-app.js');
-importScripts('https://www.gstatic.com/firebasejs/3.6.8/firebase-messaging.js');
+importScripts('https://www.gstatic.com/firebasejs/3.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.8.0/firebase-messaging.js');
 
 firebase.initializeApp({
     apiKey: 'AIzaSyBwwffZl09OqzQvu9GtOynqibef2Qh8dXc',
