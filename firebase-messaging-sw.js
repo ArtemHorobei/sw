05f4c36cdae9bcded299d1cf4c5e44f4f2f38b8f
artem.horobei <artem.horobei@yalantis.com>
2018-09-03 18:28:21 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,19 +1,19 @@
-// importScripts('https://www.gstatic.com/firebasejs/3.8.0/firebase-app.js');
-// importScripts('https://www.gstatic.com/firebasejs/3.8.0/firebase-messaging.js');
-//
-// firebase.initializeApp({
-//     apiKey: 'AIzaSyBwwffZl09OqzQvu9GtOynqibef2Qh8dXc',
-//     messagingSenderId: '512270770433'
-// });
-//
-// const messaging = firebase.messaging();
-//
-// messaging.setBackgroundMessageHandler(payload => {
-//     const title = payload.title;
-//     const options = {
-//         body: payload.body,
-//         icon: payload.icon
-//     };
-//
-//     self.registration.showNotification(title, options);
-// });
+importScripts('https://www.gstatic.com/firebasejs/3.8.0/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/3.8.0/firebase-messaging.js');
+
+firebase.initializeApp({
+    apiKey: 'AIzaSyBwwffZl09OqzQvu9GtOynqibef2Qh8dXc',
+    messagingSenderId: '512270770433'
+});
+
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(payload => {
+    const title = payload.title;
+    const options = {
+        body: payload.body,
+        icon: payload.icon
+    };
+
+    self.registration.showNotification(title, options);
+});
