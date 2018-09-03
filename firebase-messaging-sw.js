d8ce3f9989d1d16b6d3f1cf25a9b49a6cfd51f33
artem.horobei <artem.horobei@yalantis.com>
2018-09-03 13:28:14 +0300

    test

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,24 +1,44 @@
 import firebase from 'firebase/app';
 import 'firebase/messaging';
 
-firebase.initializeApp({ messagingSenderId: '512270770433' });
+// firebase.initializeApp({ messagingSenderId: '512270770433' });
+//
+// const messaging = firebase.messaging();
+//
+// const subscribe = () => {
+//     messaging.requestPermission()
+//         .then(function () {
+//             messaging.getToken()
+//                 .then(function (currentToken) {
+//                     console.log(currentToken);
+//                 })
+//                 .catch(function (err) {
+//                     console.warn('При получении токена произошла ошибка.', err);
+//                 });
+//         })
+//         .catch(function (err) {
+//             console.warn('Не удалось получить разрешение на показ уведомлений.', err);
+//         });
+// };
+//
+// subscribe();
 
-const messaging = firebase.messaging();
+importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-app.js");
+importScripts("https://www.gstatic.com/firebasejs/4.12.0/firebase-messaging.js");
 
-const subscribe = () => {
-    messaging.requestPermission()
-        .then(function () {
-            messaging.getToken()
-                .then(function (currentToken) {
-                    console.log(currentToken);
-                })
-                .catch(function (err) {
-                    console.warn('При получении токена произошла ошибка.', err);
-                });
-        })
-        .catch(function (err) {
-            console.warn('Не удалось получить разрешение на показ уведомлений.', err);
-        });
+let config = {
+    messagingSenderId: "512270770433"
 };
 
-subscribe();
+firebase.initializeApp(config);
+const messaging = firebase.messaging();
+
+messaging.setBackgroundMessageHandler(payload => {
+    const title = payload.notification.title;
+    console.log('payload', payload.notification.icon);
+    const options = {
+        body: payload.notification.body,
+        icon: payload.notification.icon
+    };
+    return self.registration.showNotification(title, options);
+});
