738f84a505e4d1c41c62bb834ea8db93283065a2
artem.horobei <artem.horobei@yalantis.com>
2018-09-03 15:02:40 +0300

    Updates

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -3,7 +3,8 @@ importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/3.5.2/firebase-messaging.js');
 
 firebase.initializeApp({
-    'messagingSenderId': '512270770433'
+    apiKey: 'AIzaSyBwwffZl09OqzQvu9GtOynqibef2Qh8dXc',
+    messagingSenderId: '512270770433'
 });
 
 const messaging = firebase.messaging();
